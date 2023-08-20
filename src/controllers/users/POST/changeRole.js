ddf97e80f77789af70f0499297ed5f85ff365da5
Martin Alba <martin_08_alba@hotmail.com>
2023-08-20 20:37:57 -0300

    feat: add logger (changeRole)

--- src/controllers/users/POST/changeRole.js
+++ src/controllers/users/POST/changeRole.js
@@ -1,4 +1,5 @@
 import UserModel from "../../../dao/models/user.model.js";
+import logger from "../../../utils/logger.js";
 
 export const changeRole = async (req, res) => {
   const { newRole } = req.body;
@@ -8,12 +9,15 @@ export const changeRole = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     if (user.role === "admin") {
+      logger.warning(`Cannot change the role of an admin - (${user.email})`);
       return res.render("errors/error-admin-role");
     }
 
     user.role = newRole;
     await user.save();
 
+    logger.info(`Changed role of user to ${newRole} - (${user.email})`);
+    
     res.redirect("/api/users");
   } catch (error) {
     console.error(error);
