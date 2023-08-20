7cba49abaf5a65bbedec1019e59c2589f4f85818
Martin Alba <martin_08_alba@hotmail.com>
2023-08-20 20:31:08 -0300

    feat: admin role error (api/users)

--- src/controllers/users/POST/changeRole.js
+++ src/controllers/users/POST/changeRole.js
@@ -2,11 +2,13 @@ import UserModel from "../../../dao/models/user.model.js";
 
 export const changeRole = async (req, res) => {
   const { newRole } = req.body;
-
   try {
     const user = await UserModel.findById(req.user._id);
     if (!user) {
       return res.status(404).json({ message: "User not found" });
+    }
+    if (user.role === "admin") {
+      return res.render("errors/error-admin-role");
     }
 
     user.role = newRole;
