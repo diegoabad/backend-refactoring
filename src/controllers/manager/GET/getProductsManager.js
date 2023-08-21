935ad294181c2e677ce3fb24b4d3176c738eac4f
Martin Alba <martin_08_alba@hotmail.com>
2023-08-21 14:42:53 -0300

    feat: (POST) updateProduct

--- src/controllers/manager/GET/getProductsManager.js
+++ src/controllers/manager/GET/getProductsManager.js
@@ -2,15 +2,28 @@ import productModel from "../../../dao/models/product.model.js";
 
 export const getProductsManager = async (req, res) => {
   const productId = req.params.productId;
+  const user = req.user;
 
   try {
     const product = await productModel.findById(productId);
+    console.log(product.owner);
+    console.log(user);
 
     if (!product) {
       return res.status(404).send("Product not found");
     }
 
-    res.render("updateProducts",  product );
+    switch (true) {
+      case product.owner === user.email:
+        return res.render("updateProducts", product);
+        
+      case product.owner == user.role:
+        return res.render("updateProducts", product);
+
+      default:
+        res.render("errors/update-error");
+    }
+
   } catch (error) {
     console.error("Error fetching product:", error);
     res.status(500).send("Error fetching product");
--- src/controllers/manager/POST/updateProduct.js
+++ src/controllers/manager/POST/updateProduct.js
@@ -3,16 +3,21 @@ import productModel from "../../../dao/models/product.model.js";
 export const updateProduct = async (req, res) => {
   const productId = req.params.productId;
   const updatedData = req.body;
-
+    
   try {
-  
-    const updatedProduct = await productModel.findByIdAndUpdate(productId, updatedData, { new: true });
+    const updatedProduct = await productModel.findByIdAndUpdate(
+      productId,
+      owner,
+      updatedData,
+      { new: true }
+      );
+
+
 
     if (!updatedProduct) {
       return res.status(404).send("Product not found");
     }
-
-    // Redirigir a una página de confirmación o a la vista de detalles actualizada
+    
     res.redirect(`/api/products/manager`);
   } catch (error) {
     console.error("Error updating product:", error);
